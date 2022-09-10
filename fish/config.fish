9ce519ec39e0133e74b1ffbf992cede779493d40
Leonardo Dapdap <leedapdap@gmail.com>
2022-09-10 09:36:38 +0200

    main some update with fish

--- fish/config.fish
+++ fish/config.fish
@@ -32,12 +32,20 @@ alias .3='cd ../../..'
 alias .4='cd ../../../..'
 alias .5='cd ../../../../..'
 
+#ranger
+alias r='ranger'
+
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
-alias l='exa -a | egrep "^\."'
+#alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+#alias la='exa -a --color=always --group-directories-first'  # all files and dirs
+#alias ll='exa -l --color=always --group-directories-first'  # long format
+#alias lt='exa -aT --color=always --group-directories-first' # tree listing
+#alias l='exa -a | egrep "^\."'
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias ll='ls -la'
+alias lt='ls --tree'
 
 # confirm before overwriting something
 alias cp="cp -i"
