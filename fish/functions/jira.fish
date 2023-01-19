4d02ad55c6e2ddcbcafa78a081bfceaa48d14f5c
Leonardo Dapdap <leedapdap@gmail.com>
2023-01-19 10:21:27 +0100

    main updated jira fish function

--- fish/functions/jira.fish
+++ fish/functions/jira.fish
@@ -1,6 +1,18 @@
 function jira
+
+ ################################################################
+ ## with this function, git branch naming convenstion would be ##
+ ## <prefix>/<ticket-number>__<description>                    ##
+ ################################################################
+
  set -l branchName (git branch --show-current)
- ## WORK_JIRA_URL should be available in conf.d of fish 
- set -l url "$WORK_JIRA_URL/browse/$branchName"
+ ## extract after prefix
+ string match -r '(?<nonSlash>[^\/]+$)' $branchName
+ ## extract before description
+ string match -r '(?<ticketNumber>^[^__]+)' $nonSlash
+ 
+ ## WORK_JIRA_URL should be available in conf.d or env of fish 
+ set -l url "$WORK_JIRA_URL/browse/$ticketNumber"
+
  open $url
 end
