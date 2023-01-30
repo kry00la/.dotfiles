a37e15ffbb7699394b15574cbf8d8ee4ad178ab2
Leonardo Dapdap <leedapdap@gmail.com>
2023-01-30 11:36:32 +0100

    remapped fzf config map and localtion

--- fish/conf.d/own.fzf.fish
+++ fish/conf.d/own.fzf.fish
@@ -1,3 +1,3 @@
-fzf_configure_bindings --git_log=\cg --directory=\cf --git_status=\ck
+fzf_configure_bindings --git_log=\cg --directory=\co --git_status=\ck
 set fzf_directory_opts --bind "enter:execute($EDITOR {} &> /dev/tty)"
 set -x fzf_git_log_opts --preview='git show {1} | delta --no-gitconfig'
