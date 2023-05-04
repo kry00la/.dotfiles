484a9e79dca7d34d4f47e9fe4d62c0ff028ac384
Leonardo Dapdap <leedapdap@gmail.com>
2023-05-04 16:16:17 +0200

    added scalaExp alias

--- fish/conf.d/own.alias.fish
+++ fish/conf.d/own.alias.fish
@@ -3,3 +3,6 @@ alias pf="fzf --preview='bat {}' --bind shift-up:preview-page-up,shift-down:prev
 ## weather
 alias weatherExtended='curl v2d.wttr.in/$WMI'
 alias weather='curl wttr.in/$WMI'
+
+## scala 
+alias scalaExp='v ~/Personal/playground/scala/playground-scala-2/src/main/scala'
